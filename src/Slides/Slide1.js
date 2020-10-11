378914fe7d3de9a6dd265940d583ea7d3b862eb8
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-11 12:28:21 +0200

    Slide1 finished

--- src/Slides/Slide1.js
+++ src/Slides/Slide1.js
@@ -1,42 +1,58 @@
 import React from "react";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 const Slide1 = () => {
   return (
     <div>
+      <h3 className="slide-title">Prednost GGL1 brave</h3>
       <div className="make-responsive">
         <img src="/images/phoneAndHands.png" alt="no house icons pict" />
       </div>
       <div className="make-responsive text-between-pictures">
         <p>
           <span className="icon">
-            <CheckBoxIcon />
-            <ArrowForwardIosIcon />
+            <CheckBoxOutlinedIcon
+              style={{ fontSize: "250%", color: "#8c8c8c" }}
+            />
+            <ArrowForwardIosIcon
+              style={{ fontSize: "250%", color: "#8c8c8c" }}
+            />
           </span>
           Simbioza softverskog rešenja i kombinacije “third-party” hardvera koji
           formira pametnu bravu
         </p>
         <p>
           <span className="icon">
-            <CheckBoxIcon />
-            <ArrowForwardIosIcon />
+            <CheckBoxOutlinedIcon
+              style={{ fontSize: "250%", color: "#e69500" }}
+            />
+            <ArrowForwardIosIcon
+              style={{ fontSize: "250%", color: "#e69500" }}
+            />
           </span>
           Bez upotrebe wireless-a i Bluetooth-a
         </p>
         <p>
           <span className="icon">
-            <CheckBoxIcon />
-            <ArrowForwardIosIcon />
+            <CheckBoxOutlinedIcon
+              style={{ fontSize: "250%", color: "#990000" }}
+            />
+            <ArrowForwardIosIcon
+              style={{ fontSize: "250%", color: "#990000" }}
+            />
           </span>
           Koriscenje NFC modula na telefonu umesto fizičkog ključa, uz back-up
           opciju korišćenja fizičkog ključa
         </p>
         <p>
           <span className="icon">
-            {/* style={{ fontSize: "80px" }} */}
-            <CheckBoxIcon className="lala" />
-            <ArrowForwardIosIcon />
+            <CheckBoxOutlinedIcon
+              style={{ fontSize: "250%", color: "#e69500" }}
+            />
+            <ArrowForwardIosIcon
+              style={{ fontSize: "250%", color: "#e69500" }}
+            />
           </span>
           Mogućnost kreiranja master ključa kao i manipulacija ostalim
           vremenskim ključevima
