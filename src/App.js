36421911e0b1342b1dcd32625a26864c83d91bf4
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-11 19:11:43 +0200

    Contact slide

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Slide3 from "./Slides/Slide3";
 import Slide6 from "./Slides/Slide6";
 import Slide10 from "./Slides/Slide10";
 import Slide11 from "./Slides/Slide11";
+import Contact from "./Slides/Contact";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Slide6 />
       <Slide10 />
       <Slide11 />
+      <Contact />
     </div>
   );
 };
---
+++ src/Slides/Contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <p style={{ fontSize: "6vw", marginBotm: "50px" }}>Contact</p>
+      <div className="logo-container">
+        <img src="/images/logo.png" alt="" />
+      </div>
+      <div className="contact-container">
+        <p className="aleksandar">Aleksandar Kovačević</p>
+        <div className="mail-container">
+          <a
+            className="email-link"
+            href="mailto:Aleksandar.Kovacevic@aol.co.uk"
+          >
+            Aleksandar.Kovacevic@aol.co.uk
+          </a>
+        </div>
+        <p className="phone-number">0694375194</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
