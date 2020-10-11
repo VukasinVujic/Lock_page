56868f57a61a065d06c8bd34303532af3a34ee7f
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-11 16:15:47 +0200

    forgot to go back two folders up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Slide1 from "./Slides/Slide1";
+import Slide3 from "./Slides/Slide3";
+import Slide6 from "./Slides/Slide6";
+import Slide10 from "./Slides/Slide10";
 
 const App = () => {
   return (
     <div>
-      <Slide1 />
+      <Slide3 />
+      <Slide6 />
+      <Slide10 />
     </div>
   );
 };
